c5843026d368e58508b10c8bf2b14a73059af20d
Sami J. Mäkinen <sjm@iki.fi>
2024-12-07 01:37:04 +0200

    more optimization

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,4 +1,5 @@
 // bin/day06.rs
+#![allow(clippy::type_complexity)]
 
 use aoc2024::*;
 use std::collections::{hash_map::Entry, HashMap};
@@ -46,79 +47,88 @@ fn main() -> anyhow::Result<()> {
     debug!("Got map:");
     print_map(&map);
 
-    let mut map_walk1 = clone_map(&map);
+    // brute force
+    // let n_block = block_guard(&map_walk1, start, direction);
+    // println!("Method 2 found blocking options: {n_block}");
+
     let start = (pos_x.unwrap(), pos_y.unwrap());
     let direction = (0, -1);
-    let (infinite, i, pos, dir) = guard_walk(&mut map_walk1, start, direction);
-    println!("Walk1: Guard walked {i} steps at {pos:?} heading to {dir:?}. Infinite loop: {infinite}");
-    println!("Walk1: visited positions# {}", count_visited(&map_walk1));
-
+    let mut walked = HashMap::new();
+    let (infinite, i, b, pos, dir) =
+        guard_walk(&mut map, &mut walked, start, direction, false);
+    println!(
+        "Walk1: Guard walked {i} steps at {pos:?} heading to {dir:?}. Infinite loop: {infinite}"
+    );
+    println!(
+        "Walk1: visited positions# {} possible block positions# {b}",
+        count_visited(&map)
+    );
     debug!("Map after walk1:");
-    print_map(&map_walk1);
-
-    let n_block = block_guard(&map_walk1, start, direction);
-    println!("Found blocking options: {n_block}");
+    print_map(&map);
 
     Ok(())
 }
 
 fn guard_walk(
     map: &mut [Vec<char>],
+    walked: &mut HashMap<((i32, i32), (i32, i32)), i32>,
     start: (i32, i32),
     direction: (i32, i32),
-) -> (bool, i32, (i32, i32), (i32, i32)) {
+    check_only: bool,
+) -> (bool, i32, i32, (i32, i32), (i32, i32)) {
     let size_x = map[0].len() as i32;
     let size_y = map.len() as i32;
     let mut pos = start;
     let mut dir = direction;
 
-    let mut walked = HashMap::new();
+    let mut blocks = 0;
     let mut i = 0;
     loop {
-        map[pos.1 as usize][pos.0 as usize] = 'X';
-        let key = (pos, dir);
-        if let Entry::Vacant(e) = walked.entry(key) {
-            e.insert(i);
-        } else {
-            // I have been here, walking to same direction!
-            return (true, i, pos, dir);
+        if !check_only {
+            map[pos.1 as usize][pos.0 as usize] = 'X';
         }
+
         let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
 
         if new_pos.0 < 0 || new_pos.0 >= size_x || new_pos.1 < 0 || new_pos.1 >= size_y {
-            return (false, i, pos, dir);
+            // just walked out of map
+            return (false, i, blocks, pos, dir);
         }
 
-        if map[new_pos.1 as usize][new_pos.0 as usize] == '#' {
+        let try_pos = &mut map[new_pos.1 as usize][new_pos.0 as usize];
+
+        if *try_pos == '#' {
             dir = turn_right(dir);
             continue;
+        }
+
+        let key = (new_pos, dir);
+        if let Entry::Vacant(e) = walked.entry(key) {
+            e.insert(i);
+        } else {
+            // I have been there, walking to same direction!
+            return (true, i, blocks, pos, dir);
+        }
+
+        if !check_only && *try_pos != '#' && *try_pos != 'X' {
+            let save = *try_pos;
+            // try to insert new block
+            *try_pos = '#';
+            debug!("Check blocking at {new_pos:?}");
+            let mut test_walked = walked.clone();
+
+            debug!("Walked size: {}", test_walked.len());
+            let (success, _, _, _, _) = guard_walk(map, &mut test_walked, pos, dir, true);
+            map[new_pos.1 as usize][new_pos.0 as usize] = save;
+            if success {
+                blocks += 1;
+                debug!("Found block position: {new_pos:?}");
+            }
         }
 
         pos = new_pos;
         i += 1;
     }
-}
-
-fn block_guard(map: &[Vec<char>], start: (i32, i32), direction: (i32, i32)) -> usize {
-    let size_x = map[0].len() as i32;
-    let size_y = map.len() as i32;
-
-    let mut n_inf = 0;
-    for y in 0..size_y {
-        for x in 0..size_x {
-            if map[y as usize][x as usize] != 'X' {
-                continue;
-            }
-            let mut try_map = clone_map(map);
-            try_map[y as usize][x as usize] = '#';
-            let (infinite, i, _pos, _dir) = guard_walk(&mut try_map, start, direction);
-            if infinite {
-                info!("Found Infinite loop, blocking ({y},{x}), steps {i}");
-                n_inf += 1;
-            }
-        }
-    }
-    n_inf
 }
 
 fn turn_right(dir: (i32, i32)) -> (i32, i32) {
@@ -133,17 +143,42 @@ fn count_visited(map: &[Vec<char>]) -> usize {
     visited
 }
 
+fn print_map(map: &[Vec<char>]) {
+    for row in map {
+        debug!("{}", row.iter().collect::<String>());
+    }
+}
+
+// brute force method, not used anymore
+#[allow(dead_code)]
+fn block_guard(map: &[Vec<char>], start: (i32, i32), direction: (i32, i32)) -> usize {
+    let size_x = map[0].len() as i32;
+    let size_y = map.len() as i32;
+
+    let mut n_inf = 0;
+    for y in 0..size_y {
+        for x in 0..size_x {
+            if map[y as usize][x as usize] != 'X' {
+                continue;
+            }
+            let mut try_map = clone_map(map);
+            try_map[y as usize][x as usize] = '#';
+            let mut walked = HashMap::new();
+            let (infinite, _, _, _, _) =
+                guard_walk(&mut try_map, &mut walked, start, direction, true);
+            if infinite {
+                n_inf += 1;
+            }
+        }
+    }
+    n_inf
+}
+
 fn clone_map(map: &[Vec<char>]) -> Vec<Vec<char>> {
     let mut new_map = Vec::with_capacity(map.len());
     map.iter().for_each(|row| {
         new_map.push(row.clone());
     });
     new_map
-}
-
-fn print_map(map: &[Vec<char>]) {
-    for row in map {
-        debug!("{}", row.iter().collect::<String>());
-    }
 }
 // EOF
