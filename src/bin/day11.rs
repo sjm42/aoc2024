e02b5bfea48c2cbf69bcf1a9e026cbad00e7aabc
Sami J. Mäkinen <sjm@iki.fi>
2024-12-11 23:22:47 +0200

    added cache, sigh

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -3,6 +3,7 @@
 use aoc2024::*;
 use num::bigint::BigUint;
 use num::Zero;
+use std::collections::HashMap;
 use std::ops::Mul;
 
 fn main() -> anyhow::Result<()> {
@@ -21,37 +22,52 @@ fn main() -> anyhow::Result<()> {
 
     debug!("Got stones: {stones:?}");
 
-    let c1 = stones.iter().map(|i| count_stones(i, 6)).sum::<u64>();
+    let mut cache = HashMap::new();
+    let c1 = stones
+        .iter()
+        .map(|i| count_stones(&mut cache, i, 6))
+        .sum::<u64>();
     println!("After 6 iterations, got {c1} stones");
 
-    let c2 = stones.iter().map(|i| count_stones(i, 25)).sum::<u64>();
+    let c2 = stones
+        .iter()
+        .map(|i| count_stones(&mut cache, i, 25))
+        .sum::<u64>();
     println!("After 25 iterations, got {c2} stones");
 
-    let c3 = stones.iter().map(|i| count_stones(i, 75)).sum::<u64>();
+    let c3 = stones
+        .iter()
+        .map(|i| count_stones(&mut cache, i, 75))
+        .sum::<u64>();
     println!("After 75 iterations, got {c3} stones");
 
     Ok(())
 }
 
 #[allow(dead_code)]
-fn count_stones(stone: &BigUint, mut n: usize) -> u64 {
+fn count_stones(cache: &mut HashMap<(BigUint, usize), u64>, stone: &BigUint, mut n: usize) -> u64 {
     if n == 0 {
         return 1;
     }
     n -= 1;
     if stone.is_zero() {
-        return count_stones(&BigUint::from(1u8), n);
+        return count_stones(cache, &BigUint::from(1u8), n);
     }
 
     let s = stone.to_string();
     let len = s.len();
     // s has even number of digits
     if len % 2 == 0 {
+        if cache.contains_key(&(stone.clone(), n)) {
+            return *cache.get(&(stone.clone(), n)).unwrap();
+        }
         let middle = len / 2;
-        return count_stones(&s[0..middle].parse::<BigUint>().unwrap(), n)
-            + count_stones(&s[middle..len].parse::<BigUint>().unwrap(), n);
+        let count = count_stones(cache, &s[0..middle].parse::<BigUint>().unwrap(), n)
+            + count_stones(cache, &s[middle..len].parse::<BigUint>().unwrap(), n);
+        cache.insert((stone.clone(), n), count);
+        return count;
     }
     debug!("At level {n}");
-    count_stones(&stone.mul(2024u32), n)
+    count_stones(cache, &stone.mul(2024u32), n)
 }
 // EOF
