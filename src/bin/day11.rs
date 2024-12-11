82102c0b75ca9706754bea0a97967cd42a9f8613
Sami J. Mäkinen <sjm@iki.fi>
2024-12-11 23:50:33 +0200

    No bignums, u128 is enough

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,10 +1,8 @@
 // bin/day11.rs
 
-use aoc2024::*;
-use num::bigint::BigUint;
-use num::Zero;
 use std::collections::HashMap;
-use std::ops::Mul;
+
+use aoc2024::*;
 
 fn main() -> anyhow::Result<()> {
     let mut opts = OptsCommon::parse();
@@ -17,57 +15,69 @@ fn main() -> anyhow::Result<()> {
 
     let stones = line
         .split_whitespace()
-        .filter_map(|s| s.parse::<u64>().ok().map(BigUint::from))
-        .collect::<Vec<BigUint>>();
+        .filter_map(|s| s.parse::<u128>().ok())
+        .collect::<Vec<u128>>();
 
     debug!("Got stones: {stones:?}");
 
     let mut cache = HashMap::new();
+    let mut max_len = 0;
     let c1 = stones
         .iter()
-        .map(|i| count_stones(&mut cache, i, 6))
+        .map(|&i| count_stones(&mut cache, i, 6, &mut max_len))
         .sum::<u64>();
     println!("After 6 iterations, got {c1} stones");
 
     let c2 = stones
         .iter()
-        .map(|i| count_stones(&mut cache, i, 25))
+        .map(|&i| count_stones(&mut cache, i, 25, &mut max_len))
         .sum::<u64>();
     println!("After 25 iterations, got {c2} stones");
 
     let c3 = stones
         .iter()
-        .map(|i| count_stones(&mut cache, i, 75))
+        .map(|&i| count_stones(&mut cache, i, 75, &mut max_len))
         .sum::<u64>();
     println!("After 75 iterations, got {c3} stones");
+
+    info!("Label max len: {max_len}");
 
     Ok(())
 }
 
 #[allow(dead_code)]
-fn count_stones(cache: &mut HashMap<(BigUint, usize), u64>, stone: &BigUint, mut n: usize) -> u64 {
+fn count_stones(
+    cache: &mut HashMap<(u128, usize), u64>,
+    stone: u128,
+    mut n: usize,
+    max_len: &mut usize,
+) -> u64 {
     if n == 0 {
         return 1;
     }
     n -= 1;
-    if stone.is_zero() {
-        return count_stones(cache, &BigUint::from(1u8), n);
+    if stone == 0 {
+        return count_stones(cache, 1, n, max_len);
     }
 
     let s = stone.to_string();
     let len = s.len();
+    if len > *max_len {
+        *max_len = len;
+    }
     // s has even number of digits
     if len % 2 == 0 {
-        if cache.contains_key(&(stone.clone(), n)) {
-            return *cache.get(&(stone.clone(), n)).unwrap();
+        if cache.contains_key(&(stone, n)) {
+            return *cache.get(&(stone, n)).unwrap();
         }
         let middle = len / 2;
-        let count = count_stones(cache, &s[0..middle].parse::<BigUint>().unwrap(), n)
-            + count_stones(cache, &s[middle..len].parse::<BigUint>().unwrap(), n);
-        cache.insert((stone.clone(), n), count);
+        let num1 = s[..middle].parse::<u128>().unwrap();
+        let num2 = s[middle..].parse::<u128>().unwrap();
+        let count = count_stones(cache, num1, n, max_len) + count_stones(cache, num2, n, max_len);
+        cache.insert((stone, n), count);
         return count;
     }
     debug!("At level {n}");
-    count_stones(cache, &stone.mul(2024u32), n)
+    count_stones(cache, stone * 2024, n, max_len)
 }
 // EOF
